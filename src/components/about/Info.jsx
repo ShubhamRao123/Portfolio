ec1a81ee2bd83fb90a5658f60c2767fc269170a7
Shubham Rao <shubhamrao834@gmail.com>
2024-06-07 19:00:24 +0530

    Add about section

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Info = () => {
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <i className="bx bx-award about__icon"></i>
+
+        <h3 className="about__title">Solved</h3>
+        <span className="about__subtitle">300+ Problems on Leetcode</span>
+      </div>
+
+      <div className="about__box">
+        <i className="bx bx-briefcase-alt about__icon"></i>
+
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">10+ Projects</span>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
